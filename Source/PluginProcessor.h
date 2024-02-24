d1a518db469eb1f1869130994731dbb07dc7525d
dawsonTolbert <tolbert_dawson@yahoo.com>
2024-02-23 23:36:33 -0500

    Added highCutFilter functionality

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -103,62 +103,26 @@ private:
     }
 
     template<typename ChainType, typename CoefficientType>
-    void updateCutFilter(ChainType& leftLowCut, const CoefficientType& cutCoefficients, const Slope& lowCutSlope) {
+    void updateCutFilter(ChainType& chain, const CoefficientType& coefficients, const Slope& slope) {
 
-        leftLowCut.template setBypassed<0>(true);
-        leftLowCut.template setBypassed<1>(true);
-        leftLowCut.template setBypassed<2>(true);
-        leftLowCut.template setBypassed<3>(true);
+        chain.template setBypassed<0>(true);
+        chain.template setBypassed<1>(true);
+        chain.template setBypassed<2>(true);
+        chain.template setBypassed<3>(true);
 
-        switch (lowCutSlope) {
+        switch (slope) {
             case Slope_48: {
-                update<3>(leftLowCut, cutCoefficients);
+                update<3>(chain, coefficients);
             }
             case Slope_36: {
-                update<2>(leftLowCut, cutCoefficients);
+                update<2>(chain, coefficients);
             }
             case Slope_24: {
-                update<1>(leftLowCut, cutCoefficients);
+                update<1>(chain, coefficients);
             }
             case Slope_12: {
-                update<0>(leftLowCut, cutCoefficients);
+                update<0>(chain, coefficients);
             }
-            //case Slope_12:
-            //{
-            //    *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            //    leftLowCut.template setBypassed<0>(false);
-            //    break;
-            //}
-            //case Slope_24:
-            //{
-            //    *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            //    leftLowCut.template setBypassed<0>(false);
-            //    *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-            //    leftLowCut.template setBypassed<1>(false);
-            //    break;
-            //}
-            //case Slope_36:
-            //{
-            //    *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            //    leftLowCut.template setBypassed<0>(false);
-            //    *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-            //    leftLowCut.template setBypassed<1>(false);
-            //    *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-            //    leftLowCut.template setBypassed<2>(false);
-            //    break;
-            //}
-            //case Slope_48:
-            //{
-            //    *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            //    leftLowCut.template setBypassed<0>(false);
-            //    *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-            //    leftLowCut.template setBypassed<1>(false);
-            //    *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-            //    leftLowCut.template setBypassed<2>(false);
-            //    *leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
-            //    leftLowCut.template setBypassed<3>(false);
-            //    break;
-            //}
         }
     }
     //==============================================================================
